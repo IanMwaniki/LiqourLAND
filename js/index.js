0a5285a5edad899085c43f02849322dc23ce438e
Ian Maina Mwaniki <ian.mwaniki@student.moringaschool.com>
2022-09-06 02:50:33 +0300

    Fetched data

--- js/index.js
+++ js/index.js
@@ -5,3 +5,33 @@ menu.addEventListener('click',function(){
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 });
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php').then((data)=>{
+    //console.log(data);
+    return data.json();
+}).then((json)=>{
+    console.log(json);
+    document.getElementById('cards').src = URL.createObjectURL(json);
+
+    // async function completedata(){
+    //     const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    //     const json = await response.json();
+    //     document.getElementById('cards').src = URL.createObjectURL(json);  
+    // }
+
+
+   let data1="";
+ completedata.map((values)=>{
+    data1+=`<div class="card">
+    <h1 class="title">${values.title}</h1>
+    <img src=${values.image} alt="img" class="images">
+    <p>${values.description}</p>
+    <p class="category">${values.category}</p>
+    <p class="price">${values.price}</p>
+</div>`
+   });
+document.getElementById("cards").innerHTML=data1
+
+}).catch((err)=>{
+    console.log(err);
+})
